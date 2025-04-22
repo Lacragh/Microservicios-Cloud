90f1da0ece6490b56409b056d322e25df519bc5c
Felipe Barreto <pipe.barreto07@gmail.com>
2025-04-22 18:01:07 -0500

    change the output file

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,4 @@
 output "public_ips" {
-  value = [
-    for vm in azurerm_linux_virtual_machine.my_terraform_vm : vm.public_ip_address
-  ]
+  value = [for vm in values(azurerm_linux_virtual_machine.my_terraform_vm) : vm.public_ip_address]
 }
+
