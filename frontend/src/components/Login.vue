c77c184c39de11dc19b98b3c4b74aa500f3c61a3
Lacragh <panterrosa90@gmail.com>
2025-04-30 08:06:07 -0500

    Update Login.vue

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -7,7 +7,7 @@
                 <div class='row'>
                     <div class='col-md-3'></div>
                     <div class='col-md-6'>
-                        <h2>MODIFICADO PRUEBA EN VIVO 30 ABRIL</h2>
+                        <h2>MODIFICADO PRUEBA EN VIVO 30 ABRIL SI SALE ESTO PETO 💀💀💀💀</h2>
                         <hr>
                     </div>
                 </div>
